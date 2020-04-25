48cf654a583647493fb4dc3917a13ae77106249b
athagiorgos <athan711@gmail.com>
2020-04-25 20:40:45 +0300

    file upload

---
+++ users.js
@@ -0,0 +1,11 @@
+module.exports = [{
+        id: 45,
+        fname: 'Gogos',
+        lname: 'Atha',
+        age: 20
+    
+//    author: 45,
+//    title: 'Gogos',
+//    price: 'Atha',
+//    genre: ''
+}];
